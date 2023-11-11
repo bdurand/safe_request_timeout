6df647c5f3d91190b97b4f2f5817c0de37a0ca1f
Brian Durand <bbdurand@gmail.com>
2023-11-11 13:06:45 -0800

    Add support for ActiveRecord 7.1

--- lib/safe_request_timeout/active_record_hook.rb
+++ lib/safe_request_timeout/active_record_hook.rb
@@ -9,8 +9,9 @@ module SafeRequestTimeout
       # @return [void]
       def add_timeout!(connection_class = nil)
         connection_class ||= ::ActiveRecord::Base.connection.class
+        exec_method = (connection_class.instance_methods.include?(:internal_exec_query) ? :internal_exec_query : :exec_query)
 
-        SafeRequestTimeout::Hooks.add_timeout!(connection_class, [:exec_query])
+        SafeRequestTimeout::Hooks.add_timeout!(connection_class, [exec_method])
 
         SafeRequestTimeout::Hooks.clear_timeout!(connection_class, [:commit_db_transaction])
       end
